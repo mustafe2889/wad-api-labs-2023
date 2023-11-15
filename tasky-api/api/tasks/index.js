8739b6a2e4a090dd3ec240fd2a5dd737c4ff7e93
Mustafe Abdi <20088394@mail.wit.ie>
2023-11-15 17:12:48 +0000

    added routing for posting a new tasks

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -17,19 +17,49 @@ router.get('/:id', (req, res) => {
     }
     return res.status(200).json(task);
 
-    router.post("/", (req, res) => {
-        const { title, description, deadline, priority, done } = req.body;
-        const newTask = {
-          id: uuidv4(),
-          title,
-          description,
-          deadline,
-          priority,
-          done,
-        };
-        tasksData.tasks.push(newTask);
-        res.status(201).json(newTask);
-        tasksData.total_results++;
-      });
+
+
+    
+});
+
+
+router.post('/', (req, res) => {
+    const { title, description, deadline, priority, done } = req.body;
+    const newTask = {
+        id: uuidv4(),
+        title,
+        description,
+        deadline,
+        priority,
+        done
+    };
+    tasksData.tasks.push(newTask);
+    res.status(201).json(newTask);
+    tasksData.total_results++;
+});
+
+
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const taskIndex = tasksData.tasks.findIndex(task => task.id === id); 
+    if (taskIndex === -1) {
+        return res.status(404).json({ status: 404, message: 'Task not found' });
+    }
+    const updatedTask = { ...tasksData.tasks[taskIndex], ...req.body, id:id };
+    tasksData.tasks[taskIndex] = updatedTask;
+    res.json(updatedTask);
+});
+
+
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    const taskIndex = tasksData.tasks.findIndex(task => task.id === id);
+
+    if (taskIndex === -1) return res.status(404).json({status:404,message:'Task not found'});
+    tasksData.tasks.splice(taskIndex, 1);
+    res.status(204).send();
+    tasksData.total_results--;
 });
 export default router;
