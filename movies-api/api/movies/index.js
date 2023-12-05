b5d49ad9058c484f36de1f8520f532b31025f73e
Mustafe Abdi <20088394@mail.wit.ie>
2023-12-05 11:56:40 +0000

    Added week 11 exercise (genres)

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,9 +1,7 @@
-import {
-    getUpcomingMovies
-  } from '../tmdb-api';
 import movieModel from './movieModel';
 import asyncHandler from 'express-async-handler';
 import express from 'express';
+import { getUpcomingMovies, getGenres } from "../tmdb-api";
 
 const router = express.Router();
 
@@ -38,4 +36,11 @@ router.get('/', asyncHandler(async (req, res) => {
     res.status(200).json(returnObject);
 }));
 
+router.get(
+    "/tmdb/genres",
+    asyncHandler(async (req, res) => {
+      const genres = await getGenres();
+      res.status(200).json(genres);
+    })
+  );
 export default router;
